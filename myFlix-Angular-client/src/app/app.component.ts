69a4150f9e266dcde266df8d3cd41e2ec1e06dfe
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-08-10 15:53:40 -0700

    6.3 adding registration and login forms

--- myFlix-Angular-client/src/app/app.component.ts
+++ myFlix-Angular-client/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+// opens user registration
+  constructor(public dialog: MatDialog) { }
+  // this function will open the dialog when the signup button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // assigning the dialog a width
+      width: '280px'
+    });
+  }
+
+  // opens user login form
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      //assign dialog width
+      width: '280px'
+    });
+  }
 }
